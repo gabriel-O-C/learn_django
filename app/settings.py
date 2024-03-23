6572fedaed9ccbe75a611dc2e7d29bdeae95f959
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-03-23 19:18:49 -0300

    style: lint

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-1z-scu)r0yfr8)8#&n-=&=+t+@)yixzwmjm(dv6^u6=+&tzfd_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "contents"
+    "contents",
 ]
 
 MIDDLEWARE = [
