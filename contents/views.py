6a51f5498bb44a705c41ec24c0b64b64e5f1229a
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-05-08 10:39:59 -0300

    feat: Create card component

--- contents/views.py
+++ contents/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
 # Create your views here.
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'contents/pages/home.html')
+    cards_list = [1,2,3,4,5,6]
+    return render(request, 'contents/pages/home.html', {'cards_list': cards_list})
