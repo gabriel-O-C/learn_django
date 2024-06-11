86677f988758e766fca435472133955eb4a60743
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-06-10 21:06:05 -0300

    use extends and block

--- contents/urls.py
+++ contents/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
 
+from .views import content_details, home
 
 urlpatterns = [
-    path('', home)
+    path('', home, name='index'),
+	path('contents/<int:id>', content_details)
 ]
--- contents/views.py
+++ contents/views.py
@@ -7,3 +7,8 @@ from django.shortcuts import render
 def home(request: HttpRequest) -> HttpResponse:
     cards_list = [1,2,3,4,5,6]
     return render(request, 'contents/pages/home.html', {'cards_list': cards_list})
+
+
+def content_details(request: HttpRequest, id: int) -> HttpResponse:
+    cards_list = [1,2,3,4,5,6]
+    return render(request, 'contents/pages/content-details.html', {'cards_list': cards_list, 'id': id})
